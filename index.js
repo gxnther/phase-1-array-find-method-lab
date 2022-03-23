ce1e206c501abc571798b47785daf22c4090d17b
Gunther <gunt.valentine@gmail.com>
2022-03-23 14:49:31 -0400

    commit tree f8171ccf11eeeaf015ee2f9bfa2a2e96e1b3260e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+
+// let winningYear = record.find(superbowlWin)
+// // function superbowlWin(record){
+//     if (record.result === "W") {
+//         return record.year
+// //    }
+// //    return winningYear ;
+// //
+
+
+
+// const record = [
+//     { year: "2015", result: "L"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "W"},
+//     //...
+//   ]
+
+function superbowlWin(record){
+    let winningRecord = record.find((gameObject) => {
+      return gameObject.result === "W"
+    })
+    // return winningRecord.year ;
+    if (winningRecord === undefined) {
+        return undefined     
+    } else {
+        return winningRecord.year 
+    }
+}
